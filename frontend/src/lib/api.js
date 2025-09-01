32ec02b4378bb0d6dbace1e47d7b5b138bde95af
TaleDipanshu <gurjardipanshu001@gmail.com>
2025-09-02 01:10:37 +0530

    Initial commit - Added backend

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://employee-management-od1a.onrender.com/api';
 
 
 // Get token from localStorage
